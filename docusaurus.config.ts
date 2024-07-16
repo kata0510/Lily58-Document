7af07ea646083652f0c256d700daca718ebe033f
kata0510 <yuchi.kbd@gmail.com>
2024-07-17 00:07:44 +0900

    fix config path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          routeBasePath: 'docs', 
+          routeBasePath: '/docs', 
         },
       } satisfies Preset.Options,
     ],
